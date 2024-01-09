2045debad1ee09a1bee0c9821d40dae4557e6ab4
CastBear <73561454+CastBear345@users.noreply.github.com>
2024-01-09 15:01:02 +0600

    #2 HomeWork

--- Program.cs
+++ Program.cs
@@ -5,7 +5,45 @@ class Program
 {
     static void Main()
     {
-        App myApp = new App("secret");
-        myApp.Startup();
+        Console.Write("Введите название текстового файла (без расширения):");
+        string fileName = Console.ReadLine();
+
+        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        string filePath = Path.Combine(desktopPath, $"{fileName}.txt");
+
+        if (File.Exists(filePath))
+        {
+            Console.WriteLine($"Файл с именем {fileName}.txt уже существует на рабочем столе. Выберите другое имя.");
+            return;
+        }
+
+        Console.WriteLine("Вы вводите строки, они будут записаны в файл. Для завершения введите 'exit' и нажмите Enter.");
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            Console.WriteLine("Введите строку для записи в файл (введите 'exit' для завершения): ");
+            while (true)
+            {
+                string input = Console.ReadLine();
+                writer.WriteLine(input);
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    break;
+                }
+            }
+        }
+
+        Console.WriteLine();
+        Console.Write("Вы уверены, что хотите создать файл? (Нажмите Enter для подтверждения)");
+        ConsoleKeyInfo confirmKey = Console.ReadKey();
+        Console.WriteLine();
+
+        if (Console.KeyAvailable && confirmKey.Key == ConsoleKey.Escape)
+        {
+            Console.WriteLine("Отмена создания файла.");
+            return;
+        }
+
+        Console.WriteLine($"Запись в файл {filePath} завершена.");
     }
 }
